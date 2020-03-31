ecc0b4d78e64ce0ea4b558a9dfb8938f4fb352f8
EresseaNui <leteneur.roland@hotmail.fr>
2020-03-31 11:27:08 +0200

    Ajout Fixture Film

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Utilisateur;
 use App\Entity\Acteur;
+use App\Entity\Film;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -35,15 +36,29 @@ class AppFixtures extends Fixture
             $utilisateurs[] = $utilisateur;
         }
 
+        $films = [];
+        for ($i = 1; $i <=75; ++$i)
+        {
+            $film = new Film();
+            $film->setTitre('Titre Film '.$i);
+            $film->setSynopsis($faker->text);
+            $film->setSlug($this->slugger->slug($film->getTitre())->lower());
+            $manager->persist($film);
+            $films[] = $film;
+        }
+
+
         $acteurs = [];
         for ($i = 1; $i <=10; ++$i)
         {
             $acteur = new Acteur();
-            $acteur->setNom('nom_acteur '.$i);
-            $acteur->setPrenom('prenom_acteur '.$i);
+            $acteur->setNom($faker->lastName);
+            $acteur->setPrenom($faker->firstName);
             $manager->persist($acteur);
             $acteurs[] = $acteur;
         }
+
+        
 
 
 
