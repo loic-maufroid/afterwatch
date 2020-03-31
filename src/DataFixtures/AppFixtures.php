30c3902474a2e855fc73393ee17f250021816083
EresseaNui <leteneur.roland@hotmail.fr>
2020-03-31 10:50:40 +0200

    Ajout Fixtures Utilisateur

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,13 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Utilisateur;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('fr_FR');
+
+        //Créer les Utilisateurs
+        $utilisateurs = [];
+        for ($i = 1; $i <=10; ++$i)
+        {
+            $utilisateur = new Utilisateur();
+            $utilisateur->setUsername('utilisateur '.$i);
+            $utilisateur->setEmail($faker->email);
+            $utilisateur->setPassword('password');
+            $manager->persist($utilisateur);
+            $utilisateurs[] = $utilisateur;
+        }
+
+
+
         // $product = new Product();
         // $manager->persist($product);
 
